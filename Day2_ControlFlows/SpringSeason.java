36be858f208c09ff63afbe3f7ab548cd3f0d7fd4
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-06-18 10:28:08 +0530

    Add NaturalNumberSum

--- Day2_ControlFlows/SpringSeason.java
+++ Day2_ControlFlows/SpringSeason.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class SpringSeason {
     public static void main(String[] args) {
-        
-        int month = Integer.parseInt(args[0]); 
-        int day = Integer.parseInt(args[1]);   
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter month (1-12): ");
+        int month = sc.nextInt();
+
+        System.out.print("Enter day: ");
+        int day = sc.nextInt();
 
         if ((month == 3 && day >= 20) || 
             (month == 4) ||
