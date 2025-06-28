0ee349f5aa59b21a1449f1dae64026289abeae4c
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-06-28 09:43:18 +0530

    Add message

---
+++ Day4_Methods/numbers.java
@@ -0,0 +1,35 @@
+package Day4_Methods;
+import java.util.*;
+public class numbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+     
+        System.out.print("Enter an integer number: ");
+        int number = scanner.nextInt();
+
+        
+        int result = checkNumber(number);
+
+      
+        if (result == 1) {
+            System.out.println("The number is Positive.");
+        } else if (result == -1) {
+            System.out.println("The number is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+
+        scanner.close();
+    }
+
+
+
+
+
+
+
+    }
+    
+}
