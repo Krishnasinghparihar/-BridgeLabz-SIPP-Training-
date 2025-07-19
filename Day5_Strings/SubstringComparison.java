312a74666ff1ac4e56a4dc8fbc5e3825a98e7f21
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-07-19 15:26:43 +0530

    Add new message

---
+++ Day5_Strings/SubstringComparison.java
@@ -0,0 +1,14 @@
+package Day5_Strings;
+
+import java.util.Scanner;
+
+public class SubstringComparison  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.println(str1.equals(str2));
+        sc.close();
+    }
+}
+
