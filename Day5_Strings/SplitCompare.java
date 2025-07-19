6e1ea79e3b08ab0d227c1096eff796e2f47b7218
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-07-19 15:23:37 +0530

    Add new message

---
+++ Day5_Strings/SplitCompare.java
@@ -0,0 +1,45 @@
+package Day5_Strings;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class SplitCompare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the text:");
+        String text = sc.nextLine();
+
+        String[] builtIn = text.split(" ");
+        String[] custom = splitManually(text);
+
+        boolean isEqual = compareArrays(builtIn, custom);
+        System.out.println("Are the arrays equal: " + isEqual);
+
+        sc.close();
+    }
+
+    public static String[] splitManually(String text) {
+        List<String> list = new ArrayList<>();
+        String word = "";
+        for (int i = 0; i <= text.length(); i++) {
+            if (i == text.length() || text.charAt(i) == ' ') {
+                if (!word.equals("")) {
+                    list.add(word);
+                }
+                word = "";
+            } else {
+                word += text.charAt(i);
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+
+    public static boolean compareArrays(String[] a, String[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (!a[i].equals(b[i])) return false;
+        }
+        return true;
+    }
+}
