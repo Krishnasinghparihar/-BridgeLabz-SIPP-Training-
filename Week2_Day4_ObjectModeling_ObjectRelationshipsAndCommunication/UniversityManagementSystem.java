76a24b1d2c62606e64aaf21ee5748219415cdd4d
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-07-22 20:12:01 +0530

    Add message

---
+++ Week2_Day4_ObjectModeling_ObjectRelationshipsAndCommunication/UniversityManagementSystem.java
@@ -0,0 +1,37 @@
+package Week2_Day4_ObjectModeling_ObjectRelationshipsAndCommunication;
+public class UniversityManagementSystem {
+    public static void main(String[] args) {
+        Student2 s = new Student2("Raj");
+        Professor p = new Professor("Dr. Singh");
+        Course2 c = new Course2("AI");
+        s.enrollCourse(c);
+        p.assignProfessor(c);
+    }
+}
+
+class Student2 {
+    String name;
+    Student2(String name) {
+        this.name = name;
+    }
+    void enrollCourse(Course2 c) {
+        System.out.println(name + " enrolled in " + c.name);
+    }
+}
+
+class Professor {
+    String name;
+    Professor(String name) {
+        this.name = name;
+    }
+    void assignProfessor(Course2 c) {
+        System.out.println(name + " assigned to " + c.name);
+    }
+}
+
+class Course2 {
+    String name;
+    Course2(String name) {
+        this.name = name;
+    }
+}
