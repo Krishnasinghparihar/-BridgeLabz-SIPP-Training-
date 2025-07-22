7670f98373028addcea7649f84c3a2224a4065ba
Krishnasinghparihar <krishnasinghparihar9258@gmail.com>
2025-07-22 19:47:51 +0530

    Add message

---
+++ Week2_Day4_ObjectModeling_ObjectRelationshipsAndCommunication/LibraryBookAggregation.java
@@ -0,0 +1,37 @@
+package Week2_Day4_ObjectModeling_ObjectRelationshipsAndCommunication;
+
+
+import java.util.ArrayList;
+
+public class LibraryBookAggregation {
+    String name;
+    ArrayList<Book> books = new ArrayList<>();
+    LibraryBookAggregation(String name) {
+        this.name = name;
+    }
+    void addBook(Book book) {
+        books.add(book);
+    }
+    void showBooks() {
+        System.out.println("Library: " + name);
+        for (Book book : books) {
+            System.out.println(book.title + " by " + book.author);
+        }
+    }
+    public static void main(String[] args) {
+        Book b1 = new Book("Java", "James");
+        Book b2 = new Book("Python", "Guido");
+        LibraryBookAggregation lib = new LibraryBookAggregation("City Library");
+        lib.addBook(b1);
+        lib.addBook(b2);
+        lib.showBooks();
+    }
+}
+
+class Book {
+    String title, author;
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
